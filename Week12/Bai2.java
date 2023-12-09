7b2d847ff55051c993b2c24355162e98d0143486
tuan15032 <nguyenducanhtuan22092004@gmail.com>
2023-12-09 14:21:52 +0700

    update

--- Week12/Bai2.java
+++ Week12/Bai2.java
@@ -1,27 +1,16 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
 
+class Singleton{
+    private static Singleton instance = null;
+    private Singleton(){};
+    public static Singleton getInstance(){
+        if (instance == null) {
+            return new Singleton();
+        }
+        return instance;
+    }
+}
 public class Bai2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int []a = new int[n];
-        for (int i=0;i<n;i++) {
-            a[i]=sc.nextInt();
-        }
-        int cnt = 0;
-        Set<Integer> set = new HashSet<>();
-        for (int i=0;i<n;i++) {
-            if(set.contains(k-a[i])) {
-                cnt++;
-                set.remove(k-a[i]);
-                set.remove(a[i]);
-                continue;
-            }
-            set.add(a[i]);
-        }
-        System.out.println(cnt);
+        Singleton x = Singleton.getInstance();
     }
 }
